b5a59356471230bde11dede77ebe095e51d9adef
David <46301704+DvdCp@users.noreply.github.com>
2021-03-31 17:43:28 +0200

    rotation system updated

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,6 +6,7 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private Transform _rotator;
     [SerializeField] private WeaponSlot _weaponSlot;
     private Vector2 movement,rotation;
+    private float angle;
     public float speed, rotationSpeed;
 
     private void Update()
@@ -14,10 +15,8 @@ public class PlayerMovement : MonoBehaviour
         //movement = movement * Time.deltaTime * speed;
         transform.Translate(movement * Time.deltaTime * speed, Space.World);
 
-        //Look around 
-        var angle = Mathf.Atan2(rotation.x, -rotation.y) * Mathf.Rad2Deg;
+        //Look around    
         _rotator.rotation = Quaternion.Euler(new Vector3(0f, 0f, angle));
-
     }
 
     private void OnTriggerEnter(Collider other) 
@@ -35,7 +34,13 @@ public class PlayerMovement : MonoBehaviour
     public void Look (InputAction.CallbackContext ctx)
     {
         var vectorRead = ctx.ReadValue<Vector2>();
-        rotation = new Vector2(vectorRead.x, vectorRead.y);
+        rotation = new Vector2(vectorRead.x, vectorRead.y); 
+        var lastPos = angle; 
+        angle = Mathf.Atan2(rotation.x, -rotation.y) * Mathf.Rad2Deg;
+        
+        // When player stops to use right stick, angle value is the last recorded
+        if(ctx.canceled)
+            angle = lastPos;
     }
     
 }
